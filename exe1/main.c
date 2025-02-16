c1d43f13072b0fd6d0221d9ef561dc24b48ec8dc
Belarminoino <matheuspb@al.insper.edu.br>
2025-02-15 21:19:40 -0300

    ex2

--- exe1/main.c
+++ exe1/main.c
@@ -3,13 +3,19 @@
 #include <stdio.h>
 
 const int BTN_PIN_R = 28;
+volatile int botao_stats= 0;
+volatile int botao_mudou =0;
+
 
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
-    printf("fall \n");
+    botao_stats = 1;
   } else if (events == 0x8) { // rise edge
-    printf("rise \n");
+    botao_stats = 0;
   }
+
+
+  botao_mudou = 1;
 }
 
 int main() {
@@ -23,5 +29,18 @@ int main() {
       BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
   while (true) {
+
+    if(botao_mudou){
+      // reseta estado do botao para o codigo verificar tudo de novo e ver se ta fall ou rise
+      botao_mudou = 0;
+
+      if (botao_stats){
+        printf("fall \n");
+
+      }
+      if(!botao_stats){
+        printf("rise \n");
+      }
+    }
   }
 }
--- exe2/main.c
+++ exe2/main.c
@@ -2,9 +2,40 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+int const botao = 28;
+int const led = 4;
+
+volatile int led_status = 0;
+
+
+void led_callback(uint gpio, uint32_t events) {
+  if (events == 0x4) { 
+    led_status = !led_status;
+  }
+
+
+}
+
 int main() {
   stdio_init_all();
 
+  gpio_init(botao);
+  gpio_set_dir(botao,GPIO_IN);
+  gpio_pull_up(botao);
+
+  gpio_set_irq_enabled_with_callback(
+    botao, GPIO_IRQ_EDGE_FALL, true, &led_callback);
+
+  gpio_init(led);
+  gpio_set_dir(led,GPIO_OUT);
+
+
   while (true) {
+    if(led_status == 1){
+      gpio_put(led,1);
+    }
+    if(led_status == 0 ){
+      gpio_put(led,0);
+    }
   }
 }
